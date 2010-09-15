f46b0f72430e217963534cf6168626073050e35e
samwho <samwho@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-15 11:07:42 +0000

    Commit for v1.2.2
    git-svn-id: https://plugins.svn.wordpress.org/lbak-google-checkout/tags/1.2.2@289717 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ php/housekeeping.php
@@ -0,0 +1,144 @@
+<?php
+function lbakgc_activation_setup() {
+    //Get database object.
+    global $wpdb;
+
+    //Check for current options.
+    $cur_options = lbakgc_get_options();
+
+
+    //If the plugin is already installed.
+    if ($cur_options) {
+        $options_exist = true;
+    }
+    else {
+        $options_exist = false;
+    }
+
+    //Declare table name.
+    $product_table_name = $wpdb->prefix . "lbakgc_product_table";
+
+    //Check table is not in use.
+    if($wpdb->get_var("SHOW TABLES LIKE '$product_table_name'") != $product_table_name) {
+        $db_product_table_exists = true;
+    }
+    else {
+        $db_product_table_exists = false;
+    }
+
+    $create_product_table_sql = "
+        CREATE TABLE $product_table_name (
+            product_id int NOT NULL AUTO_INCREMENT,
+            product_name text NOT NULL,
+            product_price text NOT NULL,
+            product_image text NOT NULL,
+            product_description text NOT NULL,
+            product_extra text NOT NULL,
+            product_shipping text NOT NULL,
+            product_category text NOT NULL,
+            multiple_pricings tinyint NOT NULL,
+            in_stock tinyint NOT NULL,
+            use_custom tinyint NOT NULL,
+            custom text NOT NULL,
+            PRIMARY KEY (product_id)
+        );
+            ";
+
+    require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+    if (function_exists('dbDelta')) {
+        dbDelta($create_product_table_sql);
+    }
+    else {
+        if ($db_product_table_exists) {
+            //This is a temporary solution to the dbDelta function not existing.
+            $wpdb->query("DROP TABLE $product_table_name");
+        }
+        $wpdb->query($create_product_table_sql);
+    }
+
+    $option_default = array(
+        'product_table_name' => $product_table_name,
+        'productWeightUnits' => 'KG', //Google has this for some reason
+        'merchant_id' => 'unset',
+        'sandbox' => '',
+        'sandbox_merchant_id' => 'unset',
+        'currency' => 'USD',
+        'highlight-time' => '5000',
+        'highlight-color' => '#FF7878',
+        'cart-opening-time' => '500',
+        'hide-cart-when-empty' => 'true',
+        'close-cart-when-click-away' => 'false',
+        'image-width' => '200px',
+        'image-height' => '200px',
+        'product-width' => '100%',
+        'product-height' => '100%',
+        'title-colour' => '#ff8800'
+    );
+
+    //BEGIN OPTION INITIALISATION LOOP
+    $options = array();
+    foreach ($option_default as $k => $v) {
+        if ($options_exist) {
+            //if the options are already set
+            if (!isset($cur_options[$k])) {
+                //Check if the option the loop is on exists
+                $options[$k] = $v;
+                //Set it to a default value if it doesn't
+            }
+            else {
+                //set it to the old value if it does already exist
+                $options[$k] = $cur_options[$k];
+            }
+        }
+        else {
+            //if option s aren't set
+            $options[$k] = $v;
+        }
+    }
+    //END OPTION INITIALISATION LOOP
+
+    //Some options housekeeping...
+    $options['version'] = $option_default['version'];
+
+    if ($options_exist) {
+        lbakgc_delete_options();
+    }
+
+    //Add AND update to account for the plugin already being there.
+    add_option('lbakgc_options', null, null, 'no');
+    lbakgc_update_options($options);
+}
+
+function lbakgc_uninstall() {
+    global $wpdb;
+
+    $options = lbakgc_get_options();
+
+    if ($options['product_table_name']) {
+        //Drop the product table.
+        $wpdb->query('DROP TABLE `' . $options['product_table_name'] . '`');
+    }
+    else {
+        //Table name option not defined... No idea why this would happen
+        //but it can't hurt to have it checked :p
+        trigger_error(__("Could not delete LBAK Google Checkout database.", 'lbakgc'), E_USER_ERROR);
+
+    }
+
+    //Erase options field in the settings table.
+    lbakgc_delete_options();
+}
+
+/*
+ * Functions to get, delete and update the lbakgc options.
+ */
+function lbakgc_get_options() {
+    return get_option('lbakgc_options');
+}
+function lbakgc_update_options($options) {
+    return update_option('lbakgc_options', $options);
+}
+function lbakgc_delete_options() {
+    return delete_option('lbakgc_options');
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,91 @@
+=== LBAK Google Checkout ===
+Contributors: samwho
+Author: Sam Rose
+Author URL: http://lbak.co.uk/
+Donate link: http://donate.lbak.co.uk/
+Tags: e-commerce, google, checkout, sell, products, easy
+Requires at least: 3.0.1
+Tested up to: 3.0.1
+Stable tag: 1.2.2
+
+A simple, easy to use Google Checkout integration WordPress plugin.
+
+== Description ==
+
+The LBAK Google Checkout plugin is aimed mainly at small projects that just want
+to sell a few things on their blog through Google Checkout and don't want too
+much else apart from ease of use.
+
+This plugin is really easy to set up and you will be selling your products
+within 20 minutes of installing it. It offers:
+
+* An easy way of integrating Google Checkout into your WordPress blog.
+* An easy way to add products to a database on your blog and display them in
+your blog posts with short codes.
+* Customisable settings and CSS to help you get the look and feel that you want.
+* An option to totally customise how your product looks using the Google
+Checkout API if you feel comfortable doing that.
+* Very little excess and clutter. 
+
+== Installation ==
+
+If you do not have a Google Checkout account already then you need to sign up
+for one before you start with this plugin. Go to http://checkout.google.com
+and follow their steps for signing up.
+
+Once you have your Google Checkout account, you want to install this plugin
+either using the WordPress installer or by downloading the .zip file and
+extracting it under wp-content/plugins/lbak-google-checkout then activate it
+in the plugins menu in your WordPress admin panel and find the Google Checkout
+link under the "Tools" menu, click on it and enter the following details:
+
+1. Your Merchant ID. This has to be set for the plugin to function. To find it,
+go to your Google Checkout account and look in the top right hand corner of the
+page, it should be there. Alternately you will find it in Settings > Profile.
+
+2. The currency you are using. It's essential that you get this right as the
+plugin will not work if you specify an incorrect currency. If you are in the UK,
+enter "GBP" (no quotes) into the box and if you are in the US enter "USD"
+(no quotes) into the box. If you want to use any other currencies you will need
+to look up how to do that in Google's documentation (I'm not sure if they have
+support for any other currencies yet).
+
+3. The rest of the settings on the Settings page can be left default if you want
+but I recommend having a play around with them to get a real feel for what they
+do and how they work. This will make it easier for you to customise how you
+want the plugin to look and feel.
+
+4. That should be it! You're now ready to start adding products to your database
+and selling things. If you have any further queries please consult the Help/FAQ
+page in the plugin's menu.
+
+== Frequently Asked Questions ==
+
+The frequently asked questions can be found either inside the plugin or at
+http://lbak.co.uk/faq.php?step=get&tag=lbakgc
+
+== Changelog ==
+
+= 1.2.2 =
+
+* Fixed a bug with the database update script.
+
+= 1.2.1 =
+
+* Another CSS bug fix. Who knows, it might work this time.
+* Added another setting to the settings panel. The ability to change the title
+colour for products.
+
+= 1.2 =
+
+* Potential bug fix loading CSS.
+* Added the ability to have a dropdown box of prices.
+
+= 1.1 =
+
+* Fixed a display bug regarding the use of USD as a currency.
+* Introduced a new interactive FAQ system.
+
+= 1.0 =
+
+* First stable release.
